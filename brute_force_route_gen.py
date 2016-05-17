d5876b7d49b9d5f2c0fd1c3f7f8bbd95170b68e3
eolecvk <eolecvk@gmail.com>
2016-05-16 21:07:09 -0400

    Update brute_force_route_gen.py

--- brute_force_route_gen.py
+++ brute_force_route_gen.py
@@ -1,8 +1,18 @@
+# Description:
+# Generate list of possible routes based on input nodes.
+# Nodes need to be parameterized with:
+#	1. Time window
+#	2. Capacity (supply/demand)
+
+# 1. Generate all the routes that take less than `TIME_LIMIT` to complete
+# 2. Filter out routes that violate truck load non-negativity constraint
+# 3. Filter out routes that violate node time-window constraint
+
 # Access path
 access_path = '/home/eolus/Dropbox/610 Project Data'
 
 # Model Parameters
-TIME_LIMIT = 4 * 3600
+TIME_LIMIT = 3300
 
 # Libraries
 import itertools
@@ -18,8 +28,7 @@ print('Building network...')
 
 g = nx.Graph()
 
-g.graph['title'] 	= 'Transportation Network v1'
-g.graph['author']	= 'Eole CERVENKA'
+g.graph['title'] 	= 'Test network'
 g.graph['day'] 		= 'Tuesday'
 g.graph['driver']	= 'John SMITH'
 g.graph['zone']		= 'East Boston'
@@ -49,20 +58,40 @@ INSTANCE_SIZE = time_matrix_array.shape[0]
 # ******************************
 load_scores = [
 0,
-10,
-10,
-10,
-10,
-10,
-10,
-10,
--10,
-10,
--10,
-10,
--10,
-10
+650,
+-614,
+137,
+532,
+-127,
+429,
+-187,
+-616,
+374,
+235,
+-178,
+-201,
+-434
 ]
+
+
+time_windows = [
+(0,86340),
+(32400, 54000), 
+(39600, 46800), 
+(28800, 54000), 
+(32400, 46800), 
+(36000, 46800), 
+(32400, 50400), 
+(32400, 50400), 
+(36000, 46800), 
+(36000, 54000), 
+(36000, 54000), 
+(32400, 61200), 
+(39600, 72000), 
+(43200, 72000)
+]
+
+
 # ******************************
 
 
@@ -70,7 +99,7 @@ load_scores = [
 
 ## Populate nodes
 for node in range(INSTANCE_SIZE):
-	g.add_node(node, load = load_scores[node])
+	g.add_node(node, load = load_scores[node], time_window = time_windows[node])
 
 ## Populate edges
 for i in range(INSTANCE_SIZE):
@@ -78,6 +107,8 @@ for i in range(INSTANCE_SIZE):
 		cost_ij = time_matrix_array[i,j]
 		g.add_edge(i,j, cost = cost_ij)
 
+
+import time
 
 # Input check
 print('**********************************')
@@ -87,40 +118,28 @@ for node in g.nodes():
 print('**********************************')
 print()
 
+input('Press [ENTER] to continue')
+
 print('**********************************')
-print('nodes w data:')
+print('Node data:')
 for node in g.nodes(data=True):
 	print('\t {node}'.format(node = node))
 print('**********************************')
 print()
 
+input('Press [ENTER] to continue')
 
 print('**********************************')
-print('edges w data:')
+print('Edge data:')
 for edge in g.edges(data=True):
 	print('\t {edge}'.format(edge = edge))
 print('**********************************')
 print()
 
+input('Press [ENTER] to continue')
 
 
-# Dynamic programming memoize function
-def memoize(f):
-    """ Memoization decorator for functions taking one or more arguments. """
-    class memodict(dict):
-        def __init__(self, f):
-            self.f = f
-        def __call__(self, *args):
-            return self[args]
-        def __missing__(self, key):
-            ret = self[key] = self.f(*key)
-            return ret
-    return memodict(f)
-
-
-@memoize
 def get_time(node1, node2):
-    """Returns time-distance between two nodes, memoizes result"""
     dist = g.edge[node1][node2]['cost']
     return int(dist)
 
@@ -131,84 +150,89 @@ def get_load_score(node1):
 	load_score = g.node[node1]['load']
 	return load_score
 
-# Returns True if load_score of node1 >0, false otherwise
-def is_supply(node1):
-	if get_load_score(node1) > 0:
-		return True
-	return False
+def get_time_window(node1):
+	time_window = g.node[node1]['time_window']
+	return time_window
 
 
-stack_size = 5
-# List of best routes found: [(route, time), (route, time), (route, time)]
-solution_stack = []
+def printRoute(route, time, fail, completion):
+	if fail == 'Load':
+		print("[{completion}%]	ROUTE: {route} | FAIL: Load constraint".format(completion= completion, route=route))
+	elif fail == 'TimeWindow':
+		print("[{completion}%]	ROUTE: {route} | \t\t\t\t FAIL: Time Window".format(completion= completion, route=route))
+	elif fail == 'Success':
+		print("[{completion}%]	ROUTE: {route} | \t\t\t\t\t\t\t TIME: {time}".format(completion= completion, route=route, time=time))
+	else:
+		print("-------------------------------------------------PROBLEM------------------------")
 
-def update_stack(stack, input, time_limit):
-	if len(stack) < stack_size:
-		stack.append(input)
-	elif input[1] < time_limit:
-		stack.pop()
-		stack.append(input)
-		stack = sorted(stack, key=lambda route: route[1])   # sort by desc time
-		time_limit = stack[-1][1]
-	return stack, time_limit
 
-def gen_optimal_routes(route_stack, time_limit):
-
-	route_stack = []
-	time_limit = 999999999999999
-
-	# Keep track of progress
-	permu_size = math.factorial(INSTANCE_SIZE-1)
-	compteur_completion	= 1
+def gen_optimal_routes():
+	
+	compteur_completion	= 0
 
 	for route in itertools.permutations(range(1, INSTANCE_SIZE)):
-		
+
 		compteur_completion	+= 1
-		second_node 	= route[1]
+		completion = round((compteur_completion/permu_size)*100,2)
+		current_time = 0
+		fail = False
+
+		second_node 	= route[0]
 		prev_node 		= 0
 		before_last_node= route[len(route)-1]
 
-		#How much do we load to last (demand) node?
-		remaining_extra_load = 0
+		current_time = get_time(before_last_node, 0)
+		current_load = 0
+		route_builder=[0]
 
-		print('COMPLETE: {completion}%'.format(completion = round((compteur_completion/permu_size)*100,2)))
+		for next in route:
+			
+			next_node = next
 
-		if not is_supply(second_node) or is_supply(before_last_node):
-			continue
 
-		else:
-			current_time 	= get_time(before_last_node, 0)
-			current_load	= 0
-			route_builder = [0]
-
-			for next in route:
+			# check for non-negativity of total current load
+			current_load += get_load_score(next_node)
+			if(current_load < 0):
+				fail = 'Load'
+				route_builder = [0]
+				route_builder += list(route)
+				route_builder.append(0)
 				
-				# check for non-negativity of total current load
-				current_load += get_load_score(next)
-				if(current_load < 0):
-					break
+				printRoute(route_builder, current_time, fail, completion)
+				break
 
-				# check for time constraint
-				current_time += get_time(prev_node, next)
-				if(current_time > time_limit):
-					break
+			current_time += get_time(prev_node, next_node)
+
+			time_window_node = get_time_window(next_node)
+			start_time_node = time_window_node[0]
+			end_time_node = time_window_node[1]
+
+			if(current_time > end_time_node):
+				fail = 'TimeWindow'
+				route_builder = [0]
+				route_builder += list(route)
+				route_builder.append(0)
 				
-				# add node to route in the making
-				route_builder.append(next)
+				printRoute(route_builder, current_time, fail, completion)
+				break
 
-				if(len(route_builder) == INSTANCE_SIZE -1):
-					route_builder.append(0)
-					remaining_extra_load = current_load
+			elif(current_time < start_time_node):
+				current_time = start_time_node
+		
+			# add node to route in the making
+			route_builder.append(next_node)
 
-					route_stack, time_limit = update_stack(route_stack, route_builder, time_limit)
+			if(len(route_builder) == INSTANCE_SIZE):
+				route_builder.append(0)
+				fail = 'Success'
+				printRoute(route_builder, current_time, fail, completion)		
+				break
 
-					print('ROUTE: {route} | TIME: {time} | {completion}% '\
-						.format(route = route_builder, time = current_time, completion = round((compteur_completion/permu_size)*100,2) ))
-					continue
+			prev_node = next_node
 
-				prev_node = next
+permu_size = math.factorial(INSTANCE_SIZE-1)
+print("Going through all {permu_size} possible routes...\n\n".format(permu_size = permu_size))
 
-	return route_stack
+input('Press [ENTER] to continue')
 
-solution_stack = gen_optimal_routes(solution_stack, TIME_LIMIT)
-print('OPTIMAL ROUTE: {routes}'.format(routes = solution_stack ))
+gen_optimal_routes()
